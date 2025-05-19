7625b2f03ded6c002e20340a08e1169481710539
JGarcia55 <j.garcia55@uniandes.edu.co>
2025-05-18 21:50:41 -0500

    Feature/gpf 93 (#31)
    * feat: Componente de rutas de entrega + tests
    
    * fix: agregar nueva variable de entorno
    
    * fix: agregar nueva variable de entorno
    
    * fix: agregar nueva variable de entorno
    
    * fix: traductor

--- app/src/components/Topbar.jsx
+++ app/src/components/Topbar.jsx
@@ -1,13 +1,51 @@
 import { Link } from "react-router";
 import Breadcrumb from "./Breadcrumb";
+import { useEffect } from 'react';
 
 export default function Topbar() {
+
+  useEffect(() => {
+    const addScript = () => {
+        const script = document.createElement('script');
+        script.src = '//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit';
+        script.async = true;
+        document.body.appendChild(script);
+    };
+
+    window.googleTranslateElementInit = () => {
+        new window.google.translate.TranslateElement({
+            pageLanguage: 'es',
+            includedLanguages: 'en,es',
+            layout: window.google.translate.TranslateElement.InlineLayout.SIMPLE,
+            autoDisplay: false, 
+        }, 'google_translate_element');
+    };
+
+    addScript();
+  }, []);
+
+    const changeLanguage = (lang) => {
+        if (lang === 'es') {
+            document.cookie = 'googtrans=;path=/;domain=' + window.location.hostname + ';expires=Thu, 01 Jan 1970 00:00:00 UTC;';
+            window.location.reload();
+        } else {
+            const domain = window.location.hostname;
+            document.cookie = `googtrans=/es/${lang};path=/;domain=${domain};`;
+            window.location.reload();
+        }
+    };
+
     return (
         <header className="flex items-center justify-between bg-gray-100 px-6 py-4 border-b h-[65px] max-h-[65px]">
             <Breadcrumb />
             <div className="flex items-center gap-4">
-                <img src="https://flagcdn.com/co.svg" alt="Colombia" className="w-5 h-5 rounded-full object-cover" />
-                <img src="https://flagcdn.com/us.svg" alt="USA" className="w-5 h-5 rounded-full object-cover" />
+                <div id="google_translate_element" style={{ display: 'none' }}></div>
+                <button onClick={() => changeLanguage('es')} className="cursor-pointer">
+                    <img src="https://flagcdn.com/co.svg" alt="Colombia" className="w-5 h-5 rounded-full object-cover" />
+                </button>
+                <button onClick={() => changeLanguage('en')} className="cursor-pointer">
+                    <img src="https://flagcdn.com/us.svg" alt="USA" className="w-5 h-5 rounded-full object-cover" />
+                </button>
                 <Link to="/logout" className="text-sm text-gray-700 hover:underline">Cerrar sesión</Link>
             </div>
         </header>
